fc705aa7cbcc602daa37fbbd0d9e10111e7a07a1
Lilian <taynaraaluna6@gmail.com>
2025-04-03 15:40:22 -0300

    desafio01

---
+++ 0001.py
@@ -0,0 +1,14 @@
+hr1 = int (input("primeiras Horas: "))
+min1 = int (input("Minutos: "))
+hr2 = int (input("Horas: "))
+min2 = int (input("minutos"))
+
+somahh = hr1 + hr2
+somaMm = min1 + min2
+
+Soma = (print("as horas: "))
+
+#tem que da as horas somadas ao resultado e da o valor da saida 
+
+
+
